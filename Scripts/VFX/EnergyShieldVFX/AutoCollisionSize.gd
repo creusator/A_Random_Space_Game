5b2ce154c4d19e6b28304fc63a33748696b2c182
creusator <elias.morin@hotmail.com>
2025-09-11 19:17:54 +0200

    VFX - improved shield shader
    - more settings to fiddle with.
    - soon capsule shapes

--- Scripts/VFX/EnergyShieldVFX/AutoCollisionSize.gd
+++ Scripts/VFX/EnergyShieldVFX/AutoCollisionSize.gd
@@ -7,22 +7,31 @@ func _ready() -> void:
 
 func _update_sprite_and_shader() -> void:
 	if collision_shape == null or collision_shape.shape == null:
-		push_error("No valid CollisionShape2D assigned")
 		return
+	
 	var shape = collision_shape.shape
 	var texture_size = texture.get_size()
-	if material == null:
-		push_error("You forgot the shader material dumbass")
+	var mat := material as ShaderMaterial
+	if mat == null:
 		return
+	
 	var new_size = Vector2.ZERO
-	if shape is RectangleShape2D:
-		new_size = shape.extents * 2.0
-	elif shape is CircleShape2D:
+	var shape_type = 0
+	var radius_px = 0.0
+	
+	if shape is CircleShape2D:
 		new_size = Vector2(shape.radius * 2.0, shape.radius * 2.0)
+		shape_type = 0
+		radius_px = shape.radius
 	elif shape is CapsuleShape2D:
-		new_size = Vector2(shape.radius * 2.0, shape.height + shape.radius * 2.0)
+		new_size = Vector2(shape.radius * 2.1, shape.height)
+		shape_type = 1
+		radius_px = shape.radius / 2
 	else:
-		push_warning("Unsupported CollisionShape2D shape")
 		return
+	
 	scale = new_size / texture_size
-	material.set_shader_parameter("shape_size", new_size)
+	
+	mat.set_shader_parameter("shape_size", new_size)
+	mat.set_shader_parameter("shape_type", shape_type)
+	mat.set_shader_parameter("radius", radius_px)
