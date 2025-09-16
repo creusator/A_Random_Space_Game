b23f3c58dd31ab07a3519f537ad7d44485321e7a
creusator <elias.morin@hotmail.com>
2025-09-16 18:36:27 +0200

    Panel shader random mode

--- Entities/Ships/Template/ShipComponents/InternalComponents/PowerPlant/PowerPlant.gd
+++ Entities/Ships/Template/ShipComponents/InternalComponents/PowerPlant/PowerPlant.gd
@@ -2,6 +2,7 @@ class_name PowerPlant
 extends ShipComponent
 
 @export var data:PowerPlantData
+@onready var sprite:Sprite2D = get_node("Sprite2D")
 
 var mass:int
 var power_generation:int
@@ -12,3 +13,9 @@ func _ready() -> void:
 	mass = data.mass
 	power_generation = data.power_generation
 	fuel_consumption = data.fuel_consumption
+
+func on_powered() -> void:
+	sprite.material.set_shader_parameter("is_active", true)
+
+func on_unpowered() -> void:
+	sprite.material.set_shader_parameter("is_active", false)
