7486558a1af492f43338882b6700db5cd1b4cc98
creusator <elias.morin@hotmail.com>
2025-10-21 12:50:02 +0200

    Player UI - Throttle
    - Throttle is now separated to get accurate precentage

--- UI/HUD/hud_ship.gd
+++ UI/HUD/hud_ship.gd
@@ -1,7 +1,8 @@
 extends Control
 
 @export var ship:Ship
-@onready var throttle: ProgressBar = $VBoxContainer/Throttle
+@onready var forward_throttle: ProgressBar = $VBoxContainer/ForwardThrottle
+@onready var backward_throttle: ProgressBar = $VBoxContainer/BackwardThrottle
 @onready var precision_mode: Label = $VBoxContainer/PrecisionMode
 
 var ship_throttle_value:float = 0.0
@@ -11,6 +12,13 @@ func _ready() -> void:
 	
 
 func _process(_delta: float) -> void:
+	if not ship:
+		return
 	ship_throttle_value = ship.get_throttle_state()
-	throttle.update(ship_throttle_value)
+	if ship_throttle_value >= 0.0:
+		forward_throttle.set_value_no_signal(ship_throttle_value*100)
+		backward_throttle.set_value_no_signal(0.0)
+	else:
+		forward_throttle.set_value_no_signal(0.0)
+		backward_throttle.set_value_no_signal(abs(ship_throttle_value)*100)
 	precision_mode.text = ("Precision mode : " + str(ship.get_precision_state()))
