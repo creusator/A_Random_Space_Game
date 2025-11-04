ee5f540259e6ea16f6963a45f262c0f737e7b823
creusator <elias.morin@hotmail.com>
2025-11-04 18:19:18 +0100

    FIX : Ship interior CCD
    The player is now accurately detecting the ships interiors Area2D

--- Entities/OnFootEntities/Player/Player.gd
+++ Entities/OnFootEntities/Player/Player.gd
@@ -5,9 +5,13 @@ extends CharacterBody2D
 @onready var player_input_component_2d: PlayerInputComponent2D = $PlayerInputComponent2D
 @onready var raycast: RayCast2D = $RayCast2D
 
-var prev_pos:Vector2
-var global_velocity:Vector2
 var sitting: bool = false
+
+var current_zone: Area2D = null
+var original_parent: Node = null
+
+func _ready() -> void:
+	original_parent = get_parent()
 
 func _process(_delta: float) -> void:
 	if Input.is_key_pressed(KEY_F):
@@ -15,6 +19,20 @@ func _process(_delta: float) -> void:
 
 func _physics_process(_delta: float) -> void:
 	global_rotation = 0.0
-	global_velocity = global_position - prev_pos
-	raycast.target_position = Vector2(global_velocity.x, global_velocity.y)
-	prev_pos = global_position
+	raycast.target_position = Vector2.ZERO
+	
+	var collider = raycast.get_collider()
+	
+	if collider != current_zone:
+		current_zone = collider
+		
+		var target_parent: Node
+		if collider:
+			target_parent = collider
+			print("Entered : ", collider.name)
+		else:
+			target_parent = original_parent
+		
+		if get_parent() != target_parent:
+			reparent(target_parent)
+			owner = target_parent
