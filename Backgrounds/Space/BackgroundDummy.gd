22d0a984af3e1511ce495584a6688c96af65f0ad
creusator <elias.morin@hotmail.com>
2025-11-18 23:40:30 +0100

    Interior camera WIP - Parallax Fix
    -Parallax background now rotates correctly, there is still tweaking needed on specific star layers.

--- Backgrounds/Space/BackgroundDummy.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-
-@onready var control: Control = $"../.."
-
-func _physics_process(delta: float) -> void:
-	control.rotation += 0.5 * delta
-	velocity.x = 150
-	rotation += 5 * delta
-	move_and_slide()
--- Backgrounds/Space/BackgroundSpace.gd
+++ Backgrounds/Space/BackgroundSpace.gd
@@ -2,6 +2,7 @@ extends ParallaxBackground
 
 @export var front_layer_enabled:bool = true
 
+@onready var skybox: ColorRect = $Skybox
 @onready var background_stars: GPUParticles2D = $Skybox/BackgroundStars
 @onready var furthest_layer: ParallaxLayer = $FurthestLayer
 @onready var furthest_stars: GPUParticles2D = $FurthestLayer/Stars
@@ -9,6 +10,12 @@ extends ParallaxBackground
 @onready var closest_stars: GPUParticles2D = $ClosestLayer/Stars
 @onready var front_layer: ParallaxLayer = $InFrontOfCamera/ParallaxLayer
 @onready var front_stars: GPUParticles2D = $InFrontOfCamera/ParallaxLayer/Stars
+
+@export var camera:Camera2D
+
+var og_furthest_layer_scale:Vector2
+var og_closest_layer_scale:Vector2
+var og_front_layer_scale:Vector2
 
 var reference_resolution: Vector2 = Vector2(1920, 1080)
 var base_particle_counts: Dictionary = {}
@@ -19,13 +26,36 @@ func _ready() -> void:
 	store_base_particle_count(furthest_stars, "furthest")
 	store_base_particle_count(closest_stars, "closest")
 	store_base_particle_count(front_stars, "front")
+	og_furthest_layer_scale = furthest_layer.motion_scale
+	og_closest_layer_scale = closest_layer.motion_scale
+	og_front_layer_scale = front_layer.motion_scale
 	get_viewport().size_changed.connect(_on_viewport_size_changed)
 	_on_viewport_size_changed()
+	update_viewport_size()
 
 func _process(_delta: float) -> void:
 	var new_size = get_viewport().size * 2
 	if Vector2i(new_size) != current_viewport_size:
 		_on_viewport_size_changed()
+	if camera:
+		furthest_layer.motion_scale = og_furthest_layer_scale
+		closest_layer.motion_scale = og_closest_layer_scale
+		front_layer.motion_scale = og_front_layer_scale
+		if rotation != 0.0:
+			furthest_layer.motion_scale = Vector2.ZERO
+			closest_layer.motion_scale = Vector2.ZERO
+			front_layer.motion_scale = Vector2.ZERO
+		rotation = -camera.rotation
+		closest_layer.rotation = -camera.rotation
+
+func update_viewport_size():
+	var viewport_size:Vector2 = Vector2(1920.0, 1080.0)
+	var center:Vector2 = viewport_size / 2
+	transform.origin = center
+	background_stars.process_material.emission_shape_scale = Vector3(viewport_size.x, viewport_size.y, 0.0) * 1.1
+	front_layer.transform.origin = center
+	front_stars.process_material.emission_shape_scale = Vector3(viewport_size.x, viewport_size.y , 0.0) * 1.1
+	
 
 func store_base_particle_count(particles: GPUParticles2D, key: String) -> void:
 	if particles:
@@ -39,6 +69,7 @@ func _on_viewport_size_changed() -> void:
 	
 	current_viewport_size = Vector2i(new_size)
 	update_parallax_layers(new_size)
+	update_viewport_size()
 
 func update_parallax_layers(viewport_size: Vector2) -> void:
 	var reference_area = reference_resolution.x * reference_resolution.y
